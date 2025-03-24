b4a98fd97175c51f4f459bc5c030922bc0d1e654
gtorrest <140654083+gtorrest@users.noreply.github.com>
2025-03-24 10:13:51 -0600

    Add files via upload

---
+++ Modulo_1_Javascript_Testing/01_sesion/scritpt.js
@@ -0,0 +1,7 @@
+var nombre = "Geovanni";
+var apellido = 'Torres';
+var nombreCompleto = `${nombre} ${apellido}`;
+var edad = 20;
+var edad2 = 10;
+var edadCompleta = edad + edad2;
+console.log(`hola mi nombre es ${nombreCompleto} y mi edad es ${edadCompleta}`);
