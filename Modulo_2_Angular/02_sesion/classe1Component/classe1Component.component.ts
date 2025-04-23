3272f05d336629ffd13c49efb386860757b754a4
gtorrest <140654083+gtorrest@users.noreply.github.com>
2025-04-23 07:07:47 -0600

    Add files via upload

---
+++ Modulo_2_Angular/02_sesion/classe1Component/classe1Component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classe1Component',
+  templateUrl: './classe1Component.component.html',
+  styleUrls: ['./classe1Component.component.css']
+})
+export class Classe1ComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
